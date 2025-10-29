6bd4f9ec1492135b28d98b7d7b1a8d2bb0d67c66
Filip Vabrousek <filipvabrousek1@gmail.com>
2025-10-29 22:57:03 +0100

    Add files via upload

---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",             // main html
+    "./src/**/*.{js,jsx,ts,tsx}" // all React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
